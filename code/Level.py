113efa19c2b488244b9600d0e65b2b04f357fbc4
Joao Vitor Alves <joao.vitor_br@hotmail.com>
2026-03-22 01:01:41 -0300

    Fixing imports in Level.py file

--- code/Level.py
+++ code/Level.py
@@ -1,5 +1,11 @@
-import pygame
+import sys
+from tkinter.font import Font
 
+import pygame
+from pygame.rect import Rect
+from pygame.surface import Surface
+
+from code.Const import COLOR_WHITE, WIN_HEIGHT
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
 
@@ -12,10 +18,31 @@ class Level:
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
+        self.timeout = 20000
 
     def run(self):
+        pygame.mixer.music.load('./asset/musica_de_fundo.mp3')
+        pygame.mixer.music.play(-1)
+        clock = pygame.time.Clock()
         while True:
+            clock.tick(58)
             for ent in self.entity_list:
                 self.window.blit(source=ent.surf, dest=ent.rect)
                 ent.move()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.level_text(14, f'{self.name} - Timeout: {self.timeout / 1000:.1f}s', COLOR_WHITE, (10, 5))
+            self.level_text(14, f'fps: {clock.get_fps():.0f}', COLOR_WHITE, (10, WIN_HEIGHT - 35))
+            self.level_text(14, f'entidades: {len(self.entity_list)}', COLOR_WHITE, (10, WIN_HEIGHT - 20))
+
             pygame.display.flip()
+
+
+    def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
+        self.window.blit(source=text_surf, dest=text_rect)
