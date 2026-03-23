77a7ca771d4136db4beeeeea5d7600516e83e67f
Joao Vitor Alves <joao.vitor_br@hotmail.com>
2026-03-23 15:45:47 -0300

    SQL3 implementation for score using proxy desing pattern

--- code/Const.py
+++ code/Const.py
@@ -76,9 +76,10 @@ ENTITY_SPEED = {
     'Enemy2': 1,
     'Enemy2Shot': 2.5,
 }
-TIMEOUT_LEVEL = 90000
+TIMEOUT_LEVEL = 10000
 TIMEOUT_STEP = 100
 EVENT_TIMEOUT = pygame.USEREVENT + 2
+
 ENTITY_HEALTH = {
     'Level1Bg0': 999,
     'Level1Bg1': 999,
@@ -100,7 +101,6 @@ ENTITY_HEALTH = {
 #M
 MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COOPERATIVE',
-
                'SCORE',
                'EXIT')
 
@@ -119,6 +119,23 @@ PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
 # S
 SPAWN_TIME = 1000
 
+
 #W
 WIN_WIDTH =669
 WIN_HEIGHT = 373
+# S
+SCORE_POS = {'Title': (WIN_WIDTH / 2, 50),
+             'EnterName': (WIN_WIDTH / 2, 80),
+             'Label': (WIN_WIDTH / 2, 90),
+             'Name': (WIN_WIDTH / 2, 110),
+             0: (WIN_WIDTH / 2, 110),
+             1: (WIN_WIDTH / 2, 130),
+             2: (WIN_WIDTH / 2, 150),
+             3: (WIN_WIDTH / 2, 170),
+             4: (WIN_WIDTH / 2, 190),
+             5: (WIN_WIDTH / 2, 210),
+             6: (WIN_WIDTH / 2, 230),
+             7: (WIN_WIDTH / 2, 250),
+             8: (WIN_WIDTH / 2, 270),
+             9: (WIN_WIDTH / 2, 290),
+             }
--- code/Game.py
+++ code/Game.py
@@ -6,6 +6,7 @@ import pygame
 from code.Const import WIN_HEIGHT, WIN_WIDTH, MENU_OPTION
 from code.Level import Level
 from code.Menu import Menu
+from code.Score import Score
 
 
 class Game:
@@ -16,14 +17,19 @@ class Game:
     def run(self):
 
         while True:
+            score = Score(self.window)
             menu = Menu(self.window)
             menu_return = menu.run()
 
-            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1],]:
                 player_score = [0,0] #[player1, player2]
                 level = Level(self.window, 'Level1', menu_return, player_score)
                 level_return = level.run(player_score)
-            elif menu_return == MENU_OPTION[4]:
+                if level_return:
+                    score.save(menu_return, player_score)
+            elif menu_return == MENU_OPTION[2]:
+                score.show()
+            elif menu_return == MENU_OPTION[3]:
                 pygame.quit() # Close Window
                 quit()        # end pygame
             else:
